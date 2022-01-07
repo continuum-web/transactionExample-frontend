5636dac47f8148c24a6e1f8935d41792f405f1e2
Scott Wears <scottwears1@outlook.com>
2022-01-07 11:55:49 +0000

    Adds more styling and buttons to credit and debit accounts

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { getSingleUser } from '../utils/apicalls';
 import '../styles/TableStyle.css';
+import '../styles/SingleUserPageStyle.css';
 
 export default function SingleUser() {
 	const { id } = useParams();
 	const [user, setUser] = useState({});
 	const [transactionsList, setTransactionsList] = useState([]);
+	const [amount, setAmount] = useState(0);
 
+	const handleSubmitCredit = event => {
+		event.preventDefault();
+		console.log('CREDIT')
+	};
+	const handleSubmitDebit = event => {
+		event.preventDefault();
+		console.log("DEBIT")
+	 }
 	useEffect(() => {
 		let isMounted = true; // note mutable flag
 
@@ -19,43 +29,64 @@ export default function SingleUser() {
 		return () => {
 			isMounted = false;
 		};
-	}, [id]);
-	//amount: 50
-	// balance: 1272;
-	// id: 'f7439e50-fb2e-4810-91d2-9846d78e302a';
-	// type: 'credit';
-	
-	
-	
+	}, []);
+
 	return (
-		<div>
-			<img src={`https://api.multiavatar.com/${user.name}.png`} alt='' />
-			<p>Name: {user.name}</p>
-			<p>Email: {user.email}</p>
-			<p>balance: {user.balance}</p>
-			<h4>Transactions</h4>
-			<table class='purpleHorizon'>
-				<thead>
-					<tr>
-						<th>Transaction Id</th>
-						<th>Amount</th>
-						<th>Balance</th>
-						<th>Type</th>
-					</tr>
-				</thead>
-				<tbody>
-					{transactionsList.map(transaction => {
-						return (
-							<tr key={transaction.id}>
-								<td>{transaction.id}</td>
-								<td>{transaction.amount}</td>
-								<td>{transaction.balance}</td>
-								<td>{transaction.type}</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
+		<div className='UserContainer'>
+			<img
+				className='UserPageProfileImg'
+				src={`https://api.multiavatar.com/${user.name}.png`}
+				alt={`of ${user.name}`}
+			/>
+			<div className='Details'>
+				<p className='UserPageName'>{user.name}</p>
+
+				<p>
+					balance: <span className='UserPageBalance'>£{user.balance}</span>
+				</p>
+				<div className='buttons'>
+					<form>
+						<input
+							type='number'
+							min='1'
+							step='any'
+							value={amount}
+							onChange={event => setAmount(event.target.value)}
+						/>
+						<button name='Credit' className='btn' onClick={handleSubmitCredit}>
+							Credit
+						</button>
+						<button name='Debit' className='btn' onClick={handleSubmitDebit}>
+							Debit
+						</button>
+					</form>
+				</div>
+			</div>
+			<div className='Table'>
+				<p className='UserPageTableTitle'>Transactions</p>
+				<table class='purpleHorizon'>
+					<thead>
+						<tr>
+							<th>Transaction Id</th>
+							<th>Amount</th>
+							<th>Balance</th>
+							<th>Type</th>
+						</tr>
+					</thead>
+					<tbody>
+						{transactionsList.map(transaction => {
+							return (
+								<tr key={transaction.id}>
+									<td>{transaction.id}</td>
+									<td>£{transaction.amount}</td>
+									<td>£{transaction.balance}</td>
+									<td>{transaction.type}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -18,3 +18,4 @@ export const getSingleUser = (id) => {
 		return user
 	});
 };
+
